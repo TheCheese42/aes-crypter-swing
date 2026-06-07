226853c3b97c6caf4c8daa915a888654a6d4e547
voidzero222 <voidzeroxdd@proton.me>
2026-06-07 20:41:14 +0200

    finalized functions in AesLib, encryption now works

--- AESCrypter/AesTest.java
+++ AESCrypter/AesTest.java
@@ -32,9 +32,31 @@ public class AesTest
         //    {"fe","2c","39","76"},
         //    {"17","b1","39","05"}
         //}));  
+        //PrintStringMatrix(AesLib.generateRoundKey(                 sucess
+        //new String[][]
+        //{
+        //    {"a0","88","23","2a"},
+        //    {"fa","54","a3","6c"},
+        //    {"fe","2c","39","76"},
+        //    {"17","b1","39","05"},
+        //},1));
         
+        PrintStringMatrix(AesLib.Encrypt(new String[][]
+        {
+        {"32","88","31","e0"},
+        {"43","5a","31","37"},
+        {"f6","30","98","07"},
+        {"a8","8d","a2","34"}
+        },
+        new String[][] 
+        {
+        {"2b","28","ab","09"},
+        {"7e","ae","f7","cf"},
+        {"15","d2","15","4f"},
+        {"16","a6","88","3c"}
+        }));
      }
-     public void PrintStringMatrix(String[][] StringIn){
+     public static void PrintStringMatrix(String[][] StringIn){
          for(int i = 0;i<StringIn.length;i++){
              for(int j = 0; j<StringIn[i].length;j++){
                  System.out.print(StringIn[i][j]+",");
