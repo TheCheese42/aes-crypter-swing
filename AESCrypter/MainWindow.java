9f9265f3ce0923f288fc7218ab43ecf6bdaa9fab
Dominik <dominik.reinartz@proton.me>
2026-06-08 00:00:33 +0200

    Implemented basic encryption in gui

--- AESCrypter/MainWindow.java
+++ AESCrypter/MainWindow.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.util.Random;
 
 public class MainWindow extends JFrame {
     private JPanel panel;
     private JButton encryptButton;
     private JButton analyseButton;
-    private JButton decryptButton;
     private JTextField messageField;
     private JButton generateButton;
     private JTextField encryptedField;
@@ -27,18 +25,37 @@ public class MainWindow extends JFrame {
     }
 
     private void setupUi() {
-        setTitle("AES Crypter");
+        setTitle("Rijndael Encryptor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(panel);
         pack();
         setVisible(true);
 
-        messageField.addActionListener(_event -> updateTable(messageTable, messageField.getText()));
+        messageField.addActionListener(_event -> updateMessageTable());
+        messageTable.getModel().addTableModelListener(_event -> updateMessageField());
         generateButton.addActionListener(_event -> generateKey());
+        encryptButton.addActionListener(_event -> encryptMessage());
     }
 
-    // Returns true if updating succeeded and false otherwise (e.g. too many characters)
+    private void updateMessageTable() {
+        String message = messageField.getText();
+        if (message.length() > 16) messageField.setText(message.substring(0, 16));
+        updateTable(messageTable, messageField.getText());
+    }
+
+    private void updateMessageField() {
+        byte[][] data = messageTable.getData();
+        char[] text = new char[16];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                text[i * 4 + j] = (char) data[i][j];
+            }
+        }
+        messageField.setText(new String(text));
+    }
+
     private boolean updateTable(HexGrid table, String textContent) {
+        // Returns true if updating succeeded and false otherwise (e.g. too many characters)
         if (textContent.length() > 16) return false;
         byte[] byteContent = new byte[16];
         for (int i = 0; i < textContent.length(); i++) {
@@ -51,10 +68,10 @@ public class MainWindow extends JFrame {
     private boolean updateTable(HexGrid table, byte[] byteContent) {
         if (byteContent.length > 16) return false;
         table.setData(new byte[][]{
-                {byteContent[0], byteContent[1], byteContent[2], byteContent[3]},
-                {byteContent[4], byteContent[5], byteContent[6], byteContent[7]},
-                {byteContent[8], byteContent[9], byteContent[10], byteContent[11]},
-                {byteContent[12], byteContent[13], byteContent[14], byteContent[15]},
+            {byteContent[0], byteContent[1], byteContent[2], byteContent[3]},
+            {byteContent[4], byteContent[5], byteContent[6], byteContent[7]},
+            {byteContent[8], byteContent[9], byteContent[10], byteContent[11]},
+            {byteContent[12], byteContent[13], byteContent[14], byteContent[15]},
         });
         return true;
     }
@@ -62,6 +79,20 @@ public class MainWindow extends JFrame {
     private void generateKey() {
         random.nextBytes(key);
         updateTable(keyTable, key);
+    }
+
+    private void encryptMessage() {
+        byte[][] data = messageTable.getData();
+        byte[][] key = keyTable.getData();
+        byte[][] encrypted = AesLib.encrypt(data, key);
+        encryptedTable.setData(encrypted);
+        String encryptedText = new String();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                encryptedText = encryptedText.concat(String.format("%02x", Math.floorMod(encrypted[i][j], 256)));
+            }
+        }
+        encryptedField.setText(encryptedText);
     }
 
     private void createUIComponents() {
